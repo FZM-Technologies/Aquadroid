658cfdc42ebd10cb9a8462c632cc402ced8bf351
Real-Troglodyte <128421931+Real-Troglodyte@users.noreply.github.com>
2023-04-01 16:43:58 +0100

    Update AquaOS.c

--- AquaOS.c
+++ AquaOS.c
@@ -1,1 +1,15 @@
-//code will go here
+#include <stdio.h>
+
+int main() {
+  int login;
+  printf("Welcome to Aquadroid");
+  printf("This is the customThemes strand");
+  printf("Loading");
+  return 0;
+  if  (login = 0)  {
+   printf("you are not logged in");
+   printf("login");
+int login = 1
+  }
+}
+ 
