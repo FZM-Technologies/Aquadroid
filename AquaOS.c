3e73effcdaad0621c14fa8f54d6c98161565dd19
❥Faimue-  ✪ <128421931+Faimue@users.noreply.github.com>
2023-04-09 10:50:46 +0100

    Revert "TasManager Debug Log Errors"

--- AquaOS.c
+++ AquaOS.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-
-int main() {
-  int login;
-  printf("Welcome to Aquadroid");
-  printf("This is the customThemes strand");
-  printf("Loading");
-  return 0;
-  if  (login = 0)  {
-   printf("you are not logged in");
-   printf("login");
-int login = 1
+//code will go here
