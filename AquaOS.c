b81050a8d08ed0642be8224bc4c69dd42ad7d8b3
❥Faimue-  ✪ <128421931+Faimue@users.noreply.github.com>
2023-04-09 15:56:50 +0100

    Update AquaOS.c

--- AquaOS.c
+++ AquaOS.c
@@ -1,1 +1,18 @@
-//code will go here
+#include <stdio.h>
+
+int main() {
+  int login;
+  printf("Welcome to Aquadroid");
+  printf("This is the customThemes strand");
+  printf("Loading");
+  return 0;
+  if  (login = 0)  {
+   printf("you are not logged in");
+   printf("login");
+int login = 1
+print(login) 
+if (login =1) {
+printf("you are now logged in");
+} 
+  }
+}
